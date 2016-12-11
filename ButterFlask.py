ea59898d3b54307ea882031c49441342d5735407
lc <kzwcoder2015@163.com>
2016-12-11 23:53:44 +0800

    defined 1 to n relationship

--- ButterFlask.py
+++ ButterFlask.py
@@ -40,12 +40,19 @@ db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'secret salt'
 
 
-#define sql 
+#define sql
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.INTEGER,primary_key=True)
     name = db.Column(db.String(64),unique=True)
+    users = db.relationship('User', backref='role')
 
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
 
 class NameForm(FlaskForm):
