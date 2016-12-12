a7aa2038d81af21076e2b4f34299def0799daac6
lc <kzwcoder2015@163.com>
2016-12-13 00:18:47 +0800

    scan the port

--- webii.py
+++ webii.py
@@ -1,6 +1,28 @@
 import os
+import socket
 
 class Domain:
-    def __init__(self,domain,port):
-        self.domain = domain;
-        self.port = port;
+    def __init__(self,domain,port,start,end):
+        self.domain = domain
+        self.port = port
+        self.start = start
+        self.end = end
+
+    def foreach(self):
+        for i in range(self.start,self.end):
+            try:
+                s = socket.socket
+
+                s.connect(('192.168.1.38',i))
+                s.send('Primal Security \n')
+                banner = s.recv(1024)
+                if banner:
+                    print("the port %d"%i)
+                s.close()
+            except:pass
+
+
+
+if __name__ == '__main__':
+    domain = Domain("192.168.1.121", 200, 1, 3000)
+    domain.foreach();
