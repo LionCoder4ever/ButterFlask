d4e7df5d437f8fd87c19670b2c632450ef678476
lc <kzwcoder2015@163.com>
2016-12-20 23:26:35 +0800

    get iterator by yield map -> put item into functions

--- webii.py
+++ webii.py
@@ -6,7 +6,7 @@ def test(f_arg, *args):
     for arg in args:
         print("another arg through *args :",arg)
 
-test('first param','python','java','ruby')
+# test('first param','python','java','ruby')
 
 #**kwargs
 #pass k-v into params
@@ -15,13 +15,43 @@ def test_kwargs(**kwargs):
     for k,v in kwargs.items():
         print("{0} <==> {1}".format(k,v))
 
-test_kwargs(name='python',another_name='ruby',first_name='java')
+# test_kwargs(name='python',another_name='ruby',first_name='java')
 
 #*args and **kwargs
 def test_args_and_kwargs(f_arg,*args,**kwargs):
     pass
 
+def generator_function():
+    for i in range(10):
+        yield i
 
+def fibo(n):
+    a = b = 1
+    for i in range(n):
+        yield a            #迭代出的结果是a
+        a,b = b,a+b        #模拟fibonacci
 
+# for item in generator_function():
+#     print(item)
+
+# for item in fibo(20):
+#     print(item)
+
+#map(func, *iterables)
+items = [1,2,3,4]
+
+squared = list(map(lambda x:x**2, items))
+print(squared)
+
+def multi(x):
+    return x*x
+def plus(x):
+    return x*2
+
+funccollection = [multi,plus]
+
+for i in range(5):
+    newsquared = list(map(lambda x: x(i), funccollection))
+    print(newsquared)
 
 
