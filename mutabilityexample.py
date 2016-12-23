83f88020bb0692fa61f55dc4e56b10883916e2a1
lc <kzwcoder2015@163.com>
2016-12-23 23:41:20 +0800

    list mutability

--- mutabilityexample.py
+++ mutabilityexample.py
@@ -9,3 +9,14 @@ bar+=['bye']
 print(bar)
 print(foo)
 
+
+# list mutability
+# be careful when def the *args
+
+def add_to(num,target=[]):
+    target.append(num)
+    return target
+
+print(add_to(1))
+print(add_to(2))
+print(add_to(3))
