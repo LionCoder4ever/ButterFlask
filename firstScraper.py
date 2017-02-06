39e2774fbea80485cb42e538c054827d72f64fc5
lc <kzwcoder2015@163.com>
2017-02-06 22:26:50 +0800

    if the page is retrieved successfully from the server, there is still the issue of the content on the page not quite being what we expected.

--- firstScraper.py
+++ firstScraper.py
@@ -1,6 +1,23 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from urllib.request import HTTPError
 
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read(),"html.parser")
-print(bsObj.h1)
+try:
+    html = urlopen("http://pythonscraping.com/pages/page1.html")
+except HTTPError as e:
+    print(e)
+else:
+    if html is None:
+        print("URL is not found")
+    else:
+        bsObj = BeautifulSoup(html.read(), "html.parser")
+        try:
+            someTag = bsObj.h1
+        except AttributeError as e:
+            print("tag was not found")
+        else:
+            if someTag is None:
+                print("tag was not found")
+            else:
+                print(someTag)
+
