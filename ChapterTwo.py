a0732f964f67966609bdcb8c332c3382c3fe334d
lc <kzwcoder2015@163.com>
2017-02-08 23:40:29 +0800

    findout the text

--- ChapterTwo.py
+++ ChapterTwo.py
@@ -4,5 +4,7 @@ from bs4 import BeautifulSoup
 html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
 bsObj = BeautifulSoup(html, "html.parser")
 nameList = bsObj.findAll("span",{"class":"green"})
+nametimes = bsObj.findAll(text="the prince")
+print(len(nametimes))
 for name in nameList:
     print(name.get_text())
