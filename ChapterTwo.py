1064edd1422eb273ee16a8f76bd752a6c8624093
lc <kzwcoder2015@163.com>
2017-02-09 00:26:53 +0800

    foreach the children of the tag

--- ChapterTwo.py
+++ ChapterTwo.py
@@ -5,6 +5,11 @@ html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
 bsObj = BeautifulSoup(html, "html.parser")
 nameList = bsObj.findAll("span",{"class":"green"})
 nametimes = bsObj.findAll(text="the prince")
-print(len(nametimes))
+# print(len(nametimes))
 for name in nameList:
-    print(name.get_text())
+    pass
+    # print(name.get_text())
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bsObj = BeautifulSoup(html, "html.parser")
+for child in bsObj.find("table",{"id":"giftList"}).children:
+    print(child)
