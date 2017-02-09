f4d7c16a8f0c95172be37cc77941e4c07ec427b3
lc <kzwcoder2015@163.com>
2017-02-09 23:41:26 +0800

    next_sibling

--- ChapterTwo.py
+++ ChapterTwo.py
@@ -13,3 +13,6 @@ html = urlopen("http://www.pythonscraping.com/pages/page3.html")
 bsObj = BeautifulSoup(html, "html.parser")
 for child in bsObj.find("table",{"id":"giftList"}).children:
     print(child)
+
+for sibling in bsObj.find("table",{"id":"giftList"}).tr.next_siblings:
+    print(sibling)
