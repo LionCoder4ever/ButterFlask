c3f76ac4a623a8554747ca393204a3d866fa3189
lc <kzwcoder2015@163.com>
2017-02-14 00:50:14 +0800

    remove the same page

--- ChapterThree.py
+++ ChapterThree.py
@@ -15,11 +15,20 @@ import datetime
 import random
 import re
 random.seed(datetime.datetime.now())
+pages = set()
 def getLinks(articleUrl):
+    global pages
     html = urlopen("http://en.wikipedia.org"+articleUrl)
     bsObj = BeautifulSoup(html)
-    return bsObj.find("div", {"id":"bodyContent"}).findAll("a",
-                         href=re.compile("^(/wiki/)((?!:).)*$"))
+    # bsObj.find("div", {"id":"bodyContent"}).findAll("a",
+    #                      href=re.compile("^(/wiki/)((?!:).)*$"))
+    for link in bsObj.findAll("a",href=re.compile("^(/wiki/)")):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                newPage = link.attrs['href']
+                print(newPage)
+                pages.add(newPage)
+                getLinks(newPage)
 links = getLinks("/wiki/Kevin_Bacon")
 while len(links) > 0:
     newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
